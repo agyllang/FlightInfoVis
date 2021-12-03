d7a68ea543d906ebb6b135c77f56a543caaab48e
bjansheden <billyjansheden@gmail.com>
2021-12-03 10:58:37 +0100

    Branch - Billy
    Established own branch

--- flight/src/App.js
+++ flight/src/App.js
@@ -131,8 +131,8 @@ const Dummy = (title) => {
       <div className="page-title"> {title} </div>
       <div className="row"></div>
       <VerticalBar
-        values={[1, 2, 3, 4, 5, 6, 7]}
-        labels={[1, 2, 3, 4, 5, 6, 7]}
+        values={[1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12]}
+        labels={["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]}
       />
     </div>
   );
