cf6c5fe527b62afe66c07e73689084fed19cf82e
bjansheden <billyjansheden@gmail.com>
2021-12-06 15:04:03 +0100

    random
    Stuff

--- flight/src/components/overview/Overview.jsx
+++ flight/src/components/overview/Overview.jsx
@@ -1,5 +1,15 @@
+<<<<<<< Updated upstream
 import React from "react";
 import FlightMap from "../map/FlightMap";
+=======
+import React, { useEffect, useState } from "react";
+// import FlightMap from "../map/FlightMap";
+import ProgressBarYearly from "../progress/progressbar";
+import Select from "react-select";
+import { Row, Col, Container, Card} from "react-bootstrap";
+
+
+>>>>>>> Stashed changes
 // import VerticalBar from "./VerticalBar";
 
 const Overview = ({ ...props }) => {
@@ -7,6 +17,7 @@ const Overview = ({ ...props }) => {
   //   console.log("Employees props", props);
 
   return (
+<<<<<<< Updated upstream
     <div className="page">
       <div className="page-title"> Overview</div>
       <div>Progress bar?</div>
@@ -57,6 +68,58 @@ const Overview = ({ ...props }) => {
       </div>
       {/* <VerticalBar values={[1,2,3,4,5,6,7]} labels={[1,2,3,4,5,6,7]}/> */}
     </div>
+=======
+    <Container className="">
+      <Row className="page-title">Overview</Row>
+      <Row>
+        <Col md={2}>
+          <Select
+            onChange={handleInputChangeYear}
+            placeholder={currentYear ? currentYear : "Choose year"}
+            options={optionsYear}
+            styles={""}
+          />
+        </Col>
+        <Col md={2}>
+          <Select
+            onChange={handleInputChangeMonth}
+            placeholder={
+              currentMonth ? optionsMonth[currentMonth].value : "Choose month"
+            }
+            options={optionsMonth}
+            styles={""}
+          />
+        </Col>
+      </Row>
+      <Row>
+      <div>Displaying data from year {currentYear} </div>
+      </Row>
+      <Row>
+      {calendar.length > 0 && (
+        <ProgressBarYearly
+          calendarSection={calendar.slice(0, currentMonth + 1)}
+          calendar={calendar}
+          selectedCO2sum={selectedCO2sum}
+          totalyearCO2={totalCO2year}
+        />
+      )}
+      </Row>
+      <Row>
+      <Col md={3}>
+      <Card style ={{width: '18rem'}}>
+        <Card.Header>{`Emitted ${currentYear}`}</Card.Header>
+        <Card.Body>{`${selectedCO2sum} CO2e/kg`}</Card.Body>
+      </Card>
+      </Col>
+      <Col md={3}>
+      <Card style ={{width: '18rem'}}>
+        <Card.Header>{`Total ${currentYear}`}</Card.Header>
+        <Card.Body>{`${totalCO2year} CO2e/kg`}</Card.Body>
+      </Card>
+      </Col>
+      </Row>
+    </Container>
+>>>>>>> Stashed changes
   );
 };
 
